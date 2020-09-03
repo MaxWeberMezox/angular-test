26ae98913a0c512afdd1f097ad9150c12c8d6929
MaxWeberMezox <smiczkij@gmail.com>
2020-09-03 15:42:58 +0300

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+let a =7;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'testapp';
 }
