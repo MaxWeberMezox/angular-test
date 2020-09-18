60f6a4834765a427f7498a6fcebd3e95b585a215
MaxWeberMezox <smiczkij@gmail.com>
2020-09-18 11:57:33 +0300

    create todo-form.components and create input and `send` button

--- src/app/shared/todos.service.ts
+++ src/app/shared/todos.service.ts
@@ -1,4 +1,8 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import construct = Reflect.construct;
+import {tap} from 'rxjs/operators';
 
 export interface Todo {
   id: number;
@@ -7,23 +11,28 @@ export interface Todo {
   date?: any;
 }
 
-setTimeout(() => {
-  this.titleClass = 'blue';
-
-  setTimeout(() => {
-    this.titleClass = 'yellow';
-  }, 2000);
-}, 2000);
-
 @Injectable({providedIn: 'root'})
 export class TodosService {
-  public todos: Todo[] = [
-    {id: 1, title: 'Buy bread', completed: false, date: new Date()},
-    {id: 2, title: 'Buy car', completed: true, date: new Date()},
-    {id: 3, title: 'Buy house', completed: false, date: new Date()},
-  ];
+  public todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=3')
+  .pipe(tap(todos => this.todos = todos));
+  }
+
   eventToggle(id: number): void {
     const idx = this.todos.findIndex(t => t.id === id);
     this.todos[idx].completed = !this.todos[idx].completed;
+  }
+
+  removeTodo(id: number): void {
+    this.todos = this.todos.filter(t => t.id !== id);
+  }
+
+  addTodo(todo: Todo): void {
+    this.todos.push(todo);
   }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {publish} from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {TodosService} from '../shared/todos.service';
+import {delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-todos',
@@ -7,21 +8,22 @@ import {publish} from 'rxjs/operators';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
-  public todosClass = 'purple';
-
-  constructor() {
-    setTimeout(() => {
-      this.todosClass = 'black';
-      setTimeout(() => {
-        this.todosClass = 'green';
-      }, 2000);
-    }, 2000);
-  }
+  public loading = true;
+  constructor(public todosService: TodosService) {
+}
 
   ngOnInit(): void {
+    this.todosService.fetchTodos()
+      .pipe(delay(2000))
+      .subscribe(() => {
+      this.loading = false;
+    });
   }
 
   onChange(id: number): void {
-    // this.eventToggle.emit(id);
+    this.todosService.eventToggle(id);
+  }
+  removeTodo(id: number): void  {
+    this.todosService.removeTodo(id);
   }
 }
